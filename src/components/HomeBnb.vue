fd27fff4ae4e19283d8a5bfc21815ae4f1cb03c2
Cianuro-83 <buzzeosimone@yahoo.com>
2023-11-03 12:37:44 +0100

    caricamento immagini home

--- src/components/HomeBnb.vue
+++ src/components/HomeBnb.vue
@@ -8,20 +8,20 @@
       <li><strong>Numero di Letti: </strong>{{ appartment.beds }}</li>
       <li><strong>Numero di Bagni: </strong>{{ appartment.bathrooms }}</li>
       <li><strong>Metri Quadri: </strong>{{ appartment.mq }}</li>
-
       <li v-if="appartment.photos && appartment.photos.length > 0">
         <strong>Foto: </strong>
-        <ul>
-          <li>{{ appartment.photos[0].photo }}</li>
-        </ul>
+        <img
+          :src="`http://127.0.0.1:8000/storage/${appartment.photos[0].path}`"
+          alt=""
+        />
       </li>
       <li v-else>
-        <strong>Foto: </strong><img src="https://picsum.photos/200/300" alt="" />
+        <strong>Foto: </strong
+        ><img src="https://picsum.photos/200/300" alt="" />
       </li>
     </ul>
   </div>
 </template>
-
 
 <script>
 import axios from "axios";
@@ -38,10 +38,12 @@ export default {
   },
   methods: {
     fechAppartments() {
-      axios.get("http://127.0.0.1:8000/api/appartments?only_sponsored=true").then((response) => {
-        this.appartments = response.data;
-        console.log(this.appartments);
-      });
+      axios
+        .get("http://127.0.0.1:8000/api/appartments?only_sponsored=true")
+        .then((response) => {
+          this.appartments = response.data;
+          console.log(this.appartments);
+        });
     },
   },
   mounted() {
