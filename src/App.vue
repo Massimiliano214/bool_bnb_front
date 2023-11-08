827c6ea05d30616f12dac9420d3f4051d7341049
Cianuro-83 <buzzeosimone@yahoo.com>
2023-11-08 17:50:40 +0100

    try component navbar

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
       <h4>HOME</h4>
     </router-link>
 <p v-if="$route.name !== 'home'" ><h3>|</h3></p>
+<router-link v-if="$route.name === 'home.message'" :to="{ name: 'home.show' }">
+      <h4> INDIETRO</h4>
+    </router-link>
+<p v-if="$route.name !== 'home.show'" ><h3>|</h3></p>
+<router-link v-if="$route.name === 'home.show'" :to="{ name: 'home.message' }">
+      <h4> CONTATTACI</h4>
+    </router-link>
+<p v-if="$route.name === 'home.show'" ><h3>|</h3></p>
     <a href="http://127.0.0.1:8000/"><h4>BACK-OFFICE</h4></a>
   </nav>
   <router-view> </router-view>
@@ -14,6 +22,10 @@
       <h4>HOME</h4>
     </router-link>
 <p v-if="$route.name !== 'home'" ><h3>|</h3></p>
+<router-link v-if="$route.name === 'home.message'" :to="{ name: 'home.show' }">
+      <h4> INDIETRO</h4>
+    </router-link>
+<p v-if="$route.name !== 'home.show'" ><h3>|</h3></p>
     <a href="http://127.0.0.1:8000/"><h4>BACK-OFFICE</h4></a>
   </nav>
   </footer>
@@ -33,31 +45,21 @@ nav {
     &:hover {
       color: rgb(122, 122, 199);
       transform: scale(1.1);
+      transform-origin: center;
       ::before {
-        content: "_____";
-        color:red;
-        transform: scale(1.1);
+        content: "";
         position: absolute;
-        top: 5px;
-        left: 5px;
-        right: 10px;
-      
-        animation: lineAnimation 0.5s ease-in-out; 
+        display: inline-block;
+        height: 2px;
+        width: 100%;
+        background-color: red;
+        top: 25px;
       }
     }
   }
   p {
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-
-  @keyframes lineAnimation {
-    0% {
-      width: 0;
-    }
-    100% {
-      width: 100px;
-    }
+    margin-left: 15px;
+    margin-right: 15px;
   }
 }
 footer {
