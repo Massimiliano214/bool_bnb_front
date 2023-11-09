036f3280852613b6e88aa0db8cee5b01581bfd6d
Cianuro-83 <buzzeosimone@yahoo.com>
2023-11-09 21:56:52 +0100

    fine ricerca base

--- src/pages/AppList.vue
+++ src/pages/AppList.vue
@@ -1,7 +1,8 @@
 <template>
-  <h1>ciao</h1>
+  <h1>RICERCA AVANZATA</h1>
   <!-- SWITCH RICERCA AVANZATA -->
-  <div class="form-check form-switch">
+  <span>solo sponsorizzati </span>
+  <div class="form-check form-switch d-inline-block">
     <input
       class="form-check-input scb-switch"
       type="checkbox"
@@ -11,7 +12,7 @@
       @change="fechAppartments"
     />
     <label class="form-check-label" for="flexSwitchCheckDefault">
-      ricerca avanzata</label
+      Tutti gli appartamenti</label
     >
   </div>
   <!-- RICERCA PER NOME APPARTAMENTO -->
@@ -55,6 +56,84 @@
       </option>
     </select>
   </div>
+  <!-- RICERCA PER METRI QUADRI -->
+  <div class="form-group mt-3">
+    <label for="mqSelect">Seleziona i metri quadri:</label>
+    <select
+      id="mqSelect"
+      class="form-select"
+      v-model="searchMq"
+      @change="searchApartments"
+    >
+      <option value="" selected>Tutti</option>
+      <option value="1-75">1 - 75 mq</option>
+      <option value="76-100">76 - 100 mq</option>
+      <option value="101-150">101 - 150 mq</option>
+      <option value="151-200">151 - 200 mq</option>
+      <option value="201-250">201 - 250 mq</option>
+      <option value="251-300">251 - 300 mq</option>
+      <option value="301-350">301 - 350 mq</option>
+      <option value="351-400">351 - 400 mq</option>
+      <option value="401-450">401 - 450 mq</option>
+      <option value="451-500">451 - 500 mq</option>
+      <option value="501+">Oltre 500 mq</option>
+    </select>
+  </div>
+  <!-- RICERCA PER NUMERO DI LETTI -->
+  <div class="form-group mt-3">
+    <label for="bedsSelect">Seleziona il numero di letti:</label>
+    <select
+      id="bedsSelect"
+      class="form-select"
+      v-model="searchBeds"
+      @change="searchApartments"
+    >
+      <option value="" selected>Tutti</option>
+      <option v-for="beds in sortedUniqueBeds" :key="beds" :value="beds">
+        {{ beds }} letti
+      </option>
+    </select>
+  </div>
+  <!-- RICERCA PER NUMERO DI BAGNI -->
+  <div class="form-group mt-3">
+    <label for="bathroomsSelect">Seleziona il numero di bagni:</label>
+    <select
+      id="bathroomsSelect"
+      class="form-select"
+      v-model="searchBathrooms"
+      @change="searchApartments"
+    >
+      <option value="" selected>Tutti</option>
+      <option
+        v-for="bathrooms in sortedUniqueBathrooms"
+        :key="bathrooms"
+        :value="bathrooms"
+      >
+        {{ bathrooms === 0 ? "0 (O)" : bathrooms }} bagni
+      </option>
+    </select>
+  </div>
+  <!-- RICERCA PER SERVIZI -->
+  <div class="form-group mt-3">
+    <label>Servizi:</label>
+    <div
+      class="form-check"
+      v-for="(serviceName, index) in services"
+      :key="index"
+    >
+      <input
+        type="checkbox"
+        class="form-check-input"
+        :id="`serviceCheckbox_${index}`"
+        :value="serviceName"
+        v-model="selectedServices"
+        @change="searchApartments"
+      />
+      <label :for="`serviceCheckbox_${index}`" class="form-check-label">{{
+        serviceName
+      }}</label>
+    </div>
+  </div>
 
   <div class="container mt-5">
     <div class="row">
@@ -88,7 +167,7 @@
             </div>
             <div class="ml-3">
               <p><strong>Indirizzo:</strong> {{ appartment.address }}</p>
-              <p><strong>Numero di Camere:</strong> {{ appartment.rooms }}</p>
+              <!-- <p><strong>Numero di Camere:</strong> {{ appartment.rooms }}</p> -->
               <p><strong>Metri Quadri:</strong> {{ appartment.mq }}</p>
             </div>
           </div>
@@ -115,6 +194,13 @@ export default {
       searchAddress: "",
       searchRooms: "",
       uniqueRooms: [],
+      searchMq: "",
+      searchBeds: "",
+      uniqueBeds: [],
+      searchBathrooms: "",
+      uniqueBathrooms: [],
+      selectedServices: [],
+      services: [],
     };
   },
   methods: {
@@ -128,16 +214,41 @@ export default {
       axios.get(apiUrl).then((response) => {
         this.allAppartments = response.data;
         this.getUniqueRooms();
+        this.getUniqueBeds();
+        this.getUniqueBathrooms();
+        this.getUniqueServices();
         this.searchApartments();
       });
     },
+    getUniqueServices() {
+      const allServices = this.allAppartments.reduce((services, appartment) => {
+        return services.concat(
+          appartment.services.map((service) => service.name)
+        );
+      }, []);
+      this.services = Array.from(new Set(allServices));
+    },
+
+    getUniqueBathrooms() {
+      const allBathrooms = this.allAppartments.map(
+        (appartment) => appartment.bathrooms
+      );
+      this.uniqueBathrooms = Array.from(
+        new Set(allBathrooms.filter((bathrooms) => bathrooms !== 0))
+      );
+    },
+    getUniqueBeds() {
+      const allBeds = this.allAppartments.map((appartment) => appartment.beds);
+      this.uniqueBeds = Array.from(new Set(allBeds.filter((bed) => bed !== 0)));
+    },
+
     getUniqueRooms() {
       const allRooms = this.allAppartments.map(
         (appartment) => appartment.rooms
       );
       this.uniqueRooms = Array.from(new Set(allRooms));
     },
-    // RICERCA PER NOME APPARTAMENTO, INDIRIZZO E NUMERO DI STANZE
+    // RICERCA PER NOME APPARTAMENTO, INDIRIZZO, STANZE, LETTI, BAGNI, MQ E SERVIZI
     searchApartments() {
       this.appartments = this.allAppartments.filter((appartment) => {
         const nameMatch =
@@ -155,9 +266,38 @@ export default {
         const roomsMatch =
           !this.searchRooms || appartment.rooms >= this.searchRooms;
 
-        return nameMatch && addressMatch && roomsMatch;
+        const mqMatch = !this.searchMq || this.checkMqRange(appartment.mq);
+
+        const bedsMatch =
+          !this.searchBeds || appartment.beds === this.searchBeds;
+
+        const bathroomsMatch =
+          !this.searchBathrooms ||
+          appartment.bathrooms === this.searchBathrooms;
+
+        const servicesMatch =
+          this.selectedServices.length === 0 ||
+          this.checkServices(appartment.services);
+
+        return (
+          nameMatch &&
+          addressMatch &&
+          roomsMatch &&
+          mqMatch &&
+          bedsMatch &&
+          bathroomsMatch &&
+          servicesMatch
+        );
       });
     },
+    checkServices(appartmentServices) {
+      return this.selectedServices.every((selectedService) =>
+        appartmentServices.some(
+          (appartmentService) => appartmentService.name === selectedService
+        )
+      );
+    },
+
     goToShowPage(id, userId) {
       this.$router.push({ name: "home.show", params: { id, user_id: userId } });
     },
@@ -167,6 +307,12 @@ export default {
       const filteredRooms = this.uniqueRooms.filter((room) => room !== 0);
 
       return filteredRooms.slice().sort((a, b) => a - b);
+    },
+    sortedUniqueBeds() {
+      return this.uniqueBeds.slice().sort((a, b) => a - b);
+    },
+    sortedUniqueBathrooms() {
+      return this.uniqueBathrooms.slice().sort((a, b) => a - b);
     },
   },
   mounted() {
